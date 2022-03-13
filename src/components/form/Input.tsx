cd1919efb02a5580aa7a614ca48b00ee9c556d72
ricardofsdomene <100056112+ricardofsdomene@users.noreply.github.com>
2022-03-13 11:34:39 -0400

    build: setting-up components

---
+++ src/components/form/Input.tsx
@@ -0,0 +1,31 @@
+import {
+  Input as ChrakraInput,
+  InputProps as ChakraInputProps,
+  FormLabel,
+  FormControl,
+} from "@chakra-ui/react";
+
+interface InputProps extends ChakraInputProps {
+  name: string;
+  label?: string;
+}
+
+export function Input({ name, label, ...rest }: InputProps){
+    return(
+        <FormControl>
+        {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+        <ChrakraInput
+          name={name}
+          id={name}
+          focusBorderColor="pink.500"
+          bg="gray.900"
+          variant="filled"
+          _hover={{
+            bgColor: "gray.900",
+          }}
+          size="lg"
+          {...rest}
+        />
+      </FormControl>
+    );
+}
