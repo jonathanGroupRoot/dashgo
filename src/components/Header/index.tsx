ffed08c2fe07cea016dc1dccbc9fe2c23e2616d3
ricardofsdomene <100056112+ricardofsdomene@users.noreply.github.com>
2022-03-13 11:49:07 -0400

    fix: header path

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,45 @@
+import {
+    Flex,
+    Input,
+    Text,
+    Icon,
+    Stack,
+    HStack,
+    Box,
+    Avatar,
+  } from "@chakra-ui/react";
+  
+  import {
+    RiNotificationLine,
+    RiSearchLine,
+    RiUserAddLine,
+  } from "react-icons/ri";
+  import { Logo } from "./Logo";
+  import { Notifications } from "./NotificationsNav";
+  import { Profile } from "./Profile";
+  import { SearchBox } from "./SearchBox";
+  
+  export function Header() {
+    return (
+      <Flex
+        as="header"
+        w="100%"
+        maxW={1480}
+        h="28"
+        mx="auto"
+        mt="4"
+        px="6"
+        align="center"
+      >
+        <Logo />
+  
+        <SearchBox />
+  
+        <Flex align="center" ml="auto">
+          <Notifications />
+          <Profile />
+        </Flex>
+      </Flex>
+    );
+  }
+  
