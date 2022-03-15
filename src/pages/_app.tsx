4ee4086028158bea307c40f849a09cba418ac0e0
ricardofsdomene <100056112+ricardofsdomene@users.noreply.github.com>
2022-03-15 19:51:51 -0400

    feat: setting-up miragejs in development

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,8 +5,13 @@ import { theme } from "../styles/theme";
 import { ChakraProvider } from "@chakra-ui/react";
 import { SidebarDrawerProvider } from "../context/SidebarDrawerContext";
 import { QueryClientProvider, QueryClient } from "react-query";
+import { makeServer } from "../services/mirage";
 
 const queryClient = new QueryClient();
+
+if (process.env.NODE_ENV === "development") {
+  makeServer();
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
